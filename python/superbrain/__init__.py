98cc22575d0bd588117480713e7ed8e2d0857267
anispy211 <anispy211@gmail.com>
2026-03-10 11:57:35 +0530

    feat(phase3): Add AutoMemoryController, LangChain adapter, and PyTorch KV-cache interceptor

--- python/superbrain/__init__.py
+++ python/superbrain/__init__.py
@@ -1,1 +1,4 @@
 from .client import Client, SuperbrainError
+from .auto import AutoMemoryController, SharedContext
+
+__all__ = ["Client", "SuperbrainError", "AutoMemoryController", "SharedContext"]
